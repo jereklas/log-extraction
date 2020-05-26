1b28697b52dc52a5d4b6b008ccd1a5a00d579610
Jeremy Klas <jeremyklas@gmail.com>
2020-05-26 16:26:50 -0700

    clean up, and store data locally so you only have to re-grab data from warcraft logs once a week

--- src/stores/raidsStore.js
+++ src/stores/raidsStore.js
@@ -9,8 +9,7 @@ const weeks = 4;
 const requestDelay = 2000;
 
 class RaidsStore {
-  raidsById = {};
-  fightsByRaidId = {};
+  raidsToGetFightsFor = [];
   parsesByRaider = {};
   zones = {};
 
@@ -44,9 +43,8 @@ class RaidsStore {
   end;
 
   constructor() {
-    // this.storageRaidsById = JSON.parse(localStorage.getItem("raids")) ?? {};
-    // this.fightsByRaidId = JSON.parse(localStorage.getItem("fights")) ?? {};
-    // this.parsesByRaider = JSON.parse(localStorage.getItem("parses")) ?? {};
+    const previouslyGrabbedRaids = JSON.parse(localStorage.getItem("raids")) ?? [];
+    this.parsesByRaider = JSON.parse(localStorage.getItem("parses")) ?? {};
 
     this.start = Date.now();
     const raidCutoff = this.start - weeks * weekInNanoSeconds;
@@ -60,139 +58,172 @@ class RaidsStore {
     request({
       url: "/reports/guild/RIVAL/Fairbanks/US",
     }).then((response) => {
-      const raidsToGetFightsFor = [];
+      const raidsById = {};
 
-      Object.values(response).forEach((raid, idx) => {
+      Object.values(response).forEach((raid) => {
         const duration = raid.end - raid.start;
 
         if (duration > 0) {
           const dayOfWeek = new Date(raid.start).getDay();
-          this.raidsById[raid.id] = raid;
+          raidsById[raid.id] = raid;
           if (raid.start > raidCutoff && (dayOfWeek === Wednesday || dayOfWeek === Thursday) && raid.zone === 1002) {
-            raidsToGetFightsFor.push(raid.id);
+            this.raidsToGetFightsFor.push(raid.id);
           }
         }
       });
 
-      const requests = [];
-      raidsToGetFightsFor.forEach((raidId) => {
-        requests.push(request({ url: `/report/fights/${raidId}` }));
+      let needToGetData = false;
+
+      this.raidsToGetFightsFor.forEach((id) => {
+        if (!previouslyGrabbedRaids.includes(id)) {
+          needToGetData = true;
+        }
       });
 
-      axios.all(requests).then(
-        axios.spread((...responses) => {
-          const fightsToGetRaidersFrom = [];
-          Object.values(this.raidsById).forEach((raid) => {
-            responses.forEach((response) => {
-              this.fightsByRaidId[raid.id] = response;
-              if (raid.start === response.start && raid.title === response.title) {
-                fightsToGetRaidersFrom.push(response);
-              }
+      console.log(needToGetData);
+      if (needToGetData) {
+        const requests = [];
+        this.raidsToGetFightsFor.forEach((raidId) => {
+          requests.push(request({ url: `/report/fights/${raidId}` }));
+        });
+
+        axios.all(requests).then(
+          axios.spread((...responses) => {
+            const fightsToGetRaidersFrom = [];
+            Object.values(raidsById).forEach((raid) => {
+              responses.forEach((response) => {
+                if (raid.start === response.start && raid.title === response.title) {
+                  fightsToGetRaidersFrom.push(response);
+                }
+              });
             });
-          });
 
-          fightsToGetRaidersFrom.forEach((fight) => {
-            fight.exportedCharacters.forEach((char) => {
-              this.parsesByRaider[char.name] = {};
+            fightsToGetRaidersFrom.forEach((fight) => {
+              fight.exportedCharacters.forEach((char) => {
+                this.parsesByRaider[char.name] = {};
+              });
             });
-          });
 
-          //send parse requests for bracket parses delayed between each other to prevent api lockout
-          const bracketRequests = [];
-          const raiders = Object.keys(this.parsesByRaider);
+            //send parse requests for bracket parses delayed between each other to prevent api lockout
+            const bracketRequests = [];
+            const raiders = Object.keys(this.parsesByRaider);
 
-          const secondsRemaining = Math.floor(((raiders.length + 3) * requestDelay) / 1000);
-          this.timeRemaining = secondsRemaining;
-          for (let i = 0; i < secondsRemaining; i++) {
+            const secondsRemaining = Math.floor(((raiders.length + 3) * requestDelay) / 1000);
+            this.timeRemaining = secondsRemaining;
+            for (let i = 0; i < secondsRemaining; i++) {
+              setTimeout(() => {
+                this.timeRemaining = this.timeRemaining - 1;
+              }, i * 1000);
+            }
+
+            for (let i = 0; i < raiders.length; i++) {
+              setTimeout(() => {
+                const raider = raiders[i];
+                bracketRequests.push(
+                  request({
+                    url: `/parses/character/${raider}/Fairbanks/US`,
+                    params: { bracket: -1, metric: this.healers.includes(raider) ? "hps" : "dps" },
+                  })
+                );
+              }, requestDelay * i);
+            }
+
+            const overallRequests = [];
+            for (let i = 0; i < raiders.length; i++) {
+              setTimeout(() => {
+                const raider = raiders[i];
+                overallRequests.push(
+                  request({
+                    url: `/parses/character/${raider}/Fairbanks/US`,
+                    params: { metric: this.healers.includes(raider) ? "hps" : "dps" },
+                  })
+                );
+              }, requestDelay * i);
+            }
+
             setTimeout(() => {
-              this.timeRemaining = this.timeRemaining - 1;
-            }, i * 1000);
-          }
+              axios
+                .all(bracketRequests)
+                .then(
+                  axios.spread((...bracketResponses) => {
+                    bracketResponses.forEach((response) => {
+                      if (response[0]) {
+                        const name = response[0].characterName;
+                        this.parsesByRaider[name].bracket = {};
 
-          for (let i = 0; i < raiders.length; i++) {
-            setTimeout(() => {
-              const raider = raiders[i];
-              bracketRequests.push(
-                request({
-                  url: `/parses/character/${raider}/Fairbanks/US`,
-                  params: { bracket: -1, metric: this.healers.includes(raider) ? "hps" : "dps" },
-                })
-              );
-            }, requestDelay * i);
-          }
+                        response.forEach((encounter) => {
+                          const { bracket } = this.parsesByRaider[name];
+                          if (!bracket[encounter.encounterID]) {
+                            bracket[encounter.encounterID] = {
+                              best: 0,
+                              parses: [],
+                            };
+                          }
 
-          const overallRequests = [];
-          for (let i = 0; i < raiders.length; i++) {
-            setTimeout(() => {
-              const raider = raiders[i];
-              overallRequests.push(
-                request({
-                  url: `/parses/character/${raider}/Fairbanks/US`,
-                  params: { metric: this.healers.includes(raider) ? "hps" : "dps" },
-                })
-              );
-            }, requestDelay * i);
-          }
+                          if (encounter.startTime > parseCutoff) {
+                            bracket[encounter.encounterID].parses.push(encounter.percentile);
+                          }
 
-          setTimeout(() => {
-            axios
-              .all(bracketRequests)
-              .then(
-                axios.spread((...bracketResponses) => {
-                  bracketResponses.forEach((response) => {
-                    if (response[0]) {
-                      const name = response[0].characterName;
-                      this.parsesByRaider[name].bracket = {};
-                      const data = response.filter((parse) => parse.startTime > parseCutoff);
-
-                      data.forEach((encounter) => {
-                        if (!this.parsesByRaider[name].bracket[encounter.encounterID]) {
-                          this.parsesByRaider[name].bracket[encounter.encounterID] = [];
-                        }
-                        this.parsesByRaider[name].bracket[encounter.encounterID].push(encounter.percentile);
-                      });
-                    }
-                  });
+                          if (bracket[encounter.encounterID].best < encounter.percentile) {
+                            bracket[encounter.encounterID].best = encounter.percentile;
+                          }
+                        });
+                      }
+                    });
+                    this.loadingBracketParses = false;
+                  })
+                )
+                .catch((errors) => {
+                  this.error =
+                    "There were too many requests made recently to WarcraftLogs. Try again in like 10 minutes to allow my api key to becoming unlocked and then try re-loading the page.";
                   this.loadingBracketParses = false;
-                })
-              )
-              .catch((errors) => {
-                this.error =
-                  "There were too many requests made recently to WarcraftLogs. Try again in like 10 minutes to allow my api key to becoming unlocked and then try re-loading the page.";
-                this.loadingBracketParses = false;
-              });
-          }, raiders.length * requestDelay);
+                });
+            }, raiders.length * requestDelay);
 
-          setTimeout(() => {
-            axios
-              .all(overallRequests)
-              .then(
-                axios.spread((...overallResponses) => {
-                  overallResponses.forEach((response) => {
-                    if (response[0]) {
-                      const name = response[0].characterName;
-                      this.parsesByRaider[name].overall = {};
-                      const data = response.filter((parse) => parse.startTime > parseCutoff);
+            setTimeout(() => {
+              axios
+                .all(overallRequests)
+                .then(
+                  axios.spread((...overallResponses) => {
+                    overallResponses.forEach((response) => {
+                      if (response[0]) {
+                        const name = response[0].characterName;
+                        this.parsesByRaider[name].overall = {};
 
-                      data.forEach((encounter) => {
-                        if (!this.parsesByRaider[name].overall[encounter.encounterID]) {
-                          this.parsesByRaider[name].overall[encounter.encounterID] = [];
-                        }
-                        this.parsesByRaider[name].overall[encounter.encounterID].push(encounter.percentile);
-                      });
-                    }
-                  });
+                        response.forEach((encounter) => {
+                          const { overall } = this.parsesByRaider[name];
+                          if (!overall[encounter.encounterID]) {
+                            overall[encounter.encounterID] = {
+                              best: 0,
+                              parses: [],
+                            };
+                          }
+
+                          if (encounter.startTime > parseCutoff) {
+                            overall[encounter.encounterID].parses.push(encounter.percentile);
+                          }
+
+                          if (overall[encounter.encounterID].best < encounter.percentile) {
+                            overall[encounter.encounterID].best = encounter.percentile;
+                          }
+                        });
+                      }
+                    });
+                    this.loadingOverallParses = false;
+                  })
+                )
+                .catch((errors) => {
+                  this.error =
+                    "There were too many requests made recently to WarcraftLogs. Try again in like 10 minutes to allow my api key to becoming unlocked and then try re-loading the page.";
                   this.loadingOverallParses = false;
-                })
-              )
-              .catch((errors) => {
-                this.error =
-                  "There were too many requests made recently to WarcraftLogs. Try again in like 10 minutes to allow my api key to becoming unlocked and then try re-loading the page.";
-                this.loadingOverallParses = false;
-              });
-          }, (raiders.length + 2) * requestDelay);
-        })
-      );
+                });
+            }, (raiders.length + 2) * requestDelay);
+          })
+        );
+      } else {
+        this.loadingBracketParses = false;
+        this.loadingOverallParses = false;
+      }
     });
   }
 
@@ -210,41 +241,31 @@ class RaidsStore {
           const bestOverallRow = { name: raider };
           const medianOverallRow = { name: raider };
 
-          if (this.parsesByRaider[raider].bracket) {
-            Object.keys(this.parsesByRaider[raider].bracket).forEach((boss) => {
-              let best = 0;
+          const { bracket } = this.parsesByRaider[raider];
+          if (bracket) {
+            Object.keys(bracket).forEach((bossKey) => {
+              const boss = bracket[bossKey];
               let average = 0;
-              let count = 0;
-              this.parsesByRaider[raider].bracket[boss].forEach((percentile) => {
-                if (percentile > best) {
-                  best = percentile;
-                }
-
+              boss.parses.forEach((percentile) => {
                 average += percentile;
-                count += 1;
               });
-              average /= count;
-              bestBracketRow[boss] = Number.isInteger(best) ? best : best.toFixed(2);
-              medianBracketRow[boss] = Number.isInteger(average) ? average : average.toFixed(2);
+              average /= boss.parses.length;
+              bestBracketRow[bossKey] = Number.isInteger(boss.best) ? boss.best : boss.best.toFixed(2);
+              medianBracketRow[bossKey] = Number.isInteger(average) ? average : average.toFixed(2);
             });
           }
 
-          if (this.parsesByRaider[raider].overall) {
-            Object.keys(this.parsesByRaider[raider].overall).forEach((boss) => {
-              let best = 0;
+          const { overall } = this.parsesByRaider[raider];
+          if (overall) {
+            Object.keys(overall).forEach((bossKey) => {
+              const boss = overall[bossKey];
               let average = 0;
-              let count = 0;
-              this.parsesByRaider[raider].overall[boss].forEach((percentile) => {
-                if (percentile > best) {
-                  best = percentile;
-                }
-
+              boss.parses.forEach((percentile) => {
                 average += percentile;
-                count += 1;
               });
-              average /= count;
-              bestOverallRow[boss] = Number.isInteger(best) ? best : best.toFixed(2);
-              medianOverallRow[boss] = Number.isInteger(average) ? average : average.toFixed(2);
+              average /= boss.parses.length;
+              bestOverallRow[bossKey] = Number.isInteger(boss.best) ? boss.best : boss.best.toFixed(2);
+              medianOverallRow[bossKey] = Number.isInteger(average) ? average : average.toFixed(2);
             });
           }
 
@@ -255,9 +276,8 @@ class RaidsStore {
         });
 
         this.loading = false;
-        // localStorage.setItem("raids", JSON.stringify(this.raidsById));
-        // localStorage.setItem("fights", JSON.stringify(this.fightsByRaidId));
-        // localStorage.setItem("parses", JSON.stringify(this.parsesByRaider));
+        localStorage.setItem("raids", JSON.stringify(this.raidsToGetFightsFor));
+        localStorage.setItem("parses", JSON.stringify(this.parsesByRaider));
       }
     }
   );
